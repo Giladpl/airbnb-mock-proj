a4cd9cae83d9ae79f04bb0e6e1afc9b50963e95d
zina <zinam84@gmail.com>
2021-05-05 19:11:13 +0300

    rate list & preview

--- src/cmps/RatePreview/RatePreview.jsx
+++ src/cmps/RatePreview/RatePreview.jsx
@@ -5,8 +5,10 @@ import './RatePreview.scss'
 export const RatePreview = ({ rate }) => {
 
     return (
-        <article className="rate-preview">
-        
+        <article className="rate-preview flex">
+            <div>{rate.property}</div>
+            <div className="bar"></div>
+            <div>{rate.val}</div>
         </article>
     )
 }
