97892c50267a0147fabf7d408fac79ee557bdda8
Gilad Peltz <gilad.peltz@gmail.com>
2021-05-06 11:25:18 +0300

    swiper

--- src/cmps/ImageSwiper/ImageSwiper.jsx
+++ src/cmps/ImageSwiper/ImageSwiper.jsx
@@ -2,15 +2,12 @@ import './ImageSwiper.scss';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
 import 'swiper/swiper.min.css';
 import 'swiper/components/pagination/pagination.min.css';
 import 'swiper/components/navigation/navigation.min.css';
 
-// import Swiper core and required modules
 import SwiperCore, { Pagination, Navigation } from 'swiper/core';
 
-// install Swiper modules
 SwiperCore.use([Pagination, Navigation]);
 
 export const ImageSwiper = ({ imgUrls }) => {
