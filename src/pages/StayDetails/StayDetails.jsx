fd8ca720bd8017b348c8d8e3bfb192ebb4d39f97
zina <zinam84@gmail.com>
2021-05-09 17:11:48 +0300

    more stay details

--- src/pages/StayDetails/StayDetails.jsx
+++ src/pages/StayDetails/StayDetails.jsx
@@ -32,16 +32,19 @@ export const StayDetails = ({ match }) => {
 
     return (
         currStay && (
-            <section className='stay-details main-layout'>
+            <section className="stay-details main-layout">
                 <h1>{currStay.name}</h1>
-                <div className='img-container grid'>
+                <div className="desc">
+                    <StarSvg fill='#FF385C' /> {avgRate} ({currStay.reviews.length} reviews) ∙ {currStay.loc.address}
+				</div>
+                <div className="img-container grid">
                     {currStay.imgUrls.map((imgUrl, idx) => (
-                        <img className={'img' + idx} key={idx} src={imgUrl} alt='' />
+                        <img className={"img" + idx} key={idx} src={imgUrl} alt='' />
                     ))}
                 </div>
                 <div className="main flex">
                     <div className="main-content">
-                        <div className='host-container flex-between'>
+                        <div className="host-container flex-between">
                             <div>
                                 <h3>Hosted by {currStay.host.fullname}</h3>
                                 <div className="property-container flex">
@@ -53,16 +56,16 @@ export const StayDetails = ({ match }) => {
                             </div>
                             <img src={currStay.host.imgUrl} alt='' />
                         </div>
-                        <div className='summary'>{currStay.summary}</div>
+                        <div className="summary">{currStay.summary}</div>
                         <AmenityList amenities={currStay.amenities} />
                     </div>
                     <CheckModal stay={currStay} avgRate={avgRate} />
                 </div>
-                <div className='reviews'>
-                    <div className='review-title'>
+                <div className="reviews">
+                    <div className="review-title">
                         <StarSvg fill='#FF385C' /> {avgRate} ({currStay.reviews.length} reviews)
 				    </div>
-                    <div className='rate-list'>
+                    <div className="rate-list">
                         <RateList rates={listAvgRate} />
                     </div>
                     <ReviewList reviews={currStay.reviews} />
