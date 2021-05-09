4e8c4efbef67cf3b5bbf34c7ade2f4771614e977
Gilad Peltz <gilad.peltz@gmail.com>
2021-05-09 20:38:47 +0300

    css filter

--- src/cmps/Filter/Filter.jsx
+++ src/cmps/Filter/Filter.jsx
@@ -6,56 +6,61 @@ import './Filter.scss';
 export const Filter = (props) => {
 	const [inputFocus, setInputFocus] = useState(null);
 
-	// useEffect(() => {
-	// 	setInputFocus(document.activeElement);
+	useEffect(() => {
+		setInputFocus(document.activeElement);
+	}, [inputFocus]);
 
-	// }, [inputFocus]);
+	const onInputFocus = () => {
+		setInputFocus(document.activeElement);
+	};
+	const onInputBlur = () => {
+		setInputFocus(null);
+	};
 
-	// const onInputFocus = () => {
-	// 	setInputFocus(document.activeElement);
-	// };
-	// const onInputBlur = () => {
-	// 	setInputFocus(document.activeElement);
-	// };
+	const setStyleOnFocus = () => {
+		// if (document.activeElement !== inputFocus)
+		// 	return { background: '#ffff' };
+		// else return { background: '#ebebeb' };
+	};
 
 	return (
 		<React.Fragment>
 			<form className='filter'>
-				<div className='search-inputs'>
+				<div className='search-inputs'  style={setStyleOnFocus()}>
 					<label htmlFor='location'> Location </label>
 					<input
-						// onFocus={onInputFocus}
-						// onBlur={onInputBlur}
+						onFocus={onInputFocus}
+						onBlur={onInputBlur}
 						name='location'
 						id='location'
 						placeholder='Where are you going?'
 					/>
 				</div>
-				<div className='search-inputs'>
+				<div className='search-inputs' style={setStyleOnFocus()}>
 					<label htmlFor='check-in'>Check in</label>
 					<input
-						// onFocus={onInputFocus}
-						// onBlur={onInputBlur}
+						onFocus={onInputFocus}
+						onBlur={onInputBlur}
 						name='check-in'
 						id='check-in'
 						placeholder='Add dates'
 					/>
 				</div>
-				<div className='search-inputs'>
+				<div className='search-inputs' style={setStyleOnFocus()}>
 					<label htmlFor='check-out'>Check out</label>
 					<input
-						// onFocus={onInputFocus}
-						// onBlur={onInputBlur}
+						onFocus={onInputFocus}
+						onBlur={onInputBlur}
 						name='check-out'
 						id='check-out'
 						placeholder='Add dates'
 					/>
 				</div>
-				<div className='search-inputs'>
+				<div className='search-inputs' style={setStyleOnFocus()}>
 					<label htmlFor='guests'>Guests</label>
 					<input
-						// onFocus={onInputFocus}
-						// onBlur={onInputBlur}
+						onFocus={onInputFocus}
+						onBlur={onInputBlur}
 						name='guests'
 						id='guests'
 						placeholder='Add guests'
