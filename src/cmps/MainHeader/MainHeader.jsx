e55ee35d004aa2b2e54ea18fd0331bfcf7486e28
Gilad Peltz <gilad.peltz@gmail.com>
2021-05-13 11:24:13 +0300

    filter transition

--- src/cmps/MainHeader/MainHeader.jsx
+++ src/cmps/MainHeader/MainHeader.jsx
@@ -1,12 +1,24 @@
+import { ReactComponent as MagnifyingGlass } from '../../assets/svgs/magnifying-glass.svg';
 import './MainHeader.scss';
 
 export const MainHeader = ({ isMainHeader }) => {
 	const covidHeader = <p>Get the latest on our COVID-19 response</p>;
-	// const mainHeader;
+	const mainHeader = (
+		<form className='search-form flex'>
+			<input type='text' placeholder='Start your search'/>
+			<div className='search-button'>
+				<button className='flex-center'>
+					<MagnifyingGlass fill='white' />
+				</button>
+			</div>
+		</form>
+	);
 
 	return (
-		<header className= {isMainHeader ? 'main-header' : 'covid-header flex-center'}>
-			{isMainHeader ? 'Main Header' : covidHeader}
+		<header
+			className={isMainHeader ? 'main-header flex-center' : 'covid-header flex-center'}
+		>
+			{isMainHeader ? mainHeader : covidHeader}
 		</header>
 	);
 };
