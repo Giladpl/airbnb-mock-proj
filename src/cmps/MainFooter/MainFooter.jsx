ca51fd651967749f0a17deb629519770b594a18d
zina <zinam84@gmail.com>
2021-05-20 13:36:21 +0300

    footer

--- src/cmps/MainFooter/MainFooter.jsx
+++ src/cmps/MainFooter/MainFooter.jsx
@@ -1,12 +1,18 @@
-
-
 import './MainFooter.scss'
+import facebook from '../../assets/img/facebook.svg';
+import instagram from '../../assets/img/instagram.svg';
+import twitter from '../../assets/img/twitter.svg';
 
 export const MainFooter = (props) => {
 
     return (
-        <footer>
-        I'm the footer
+        <footer className="footer flex-between">
+            <div>© 2021 </div>
+            <div className="flex">
+                <img src={facebook} alt="" />
+                <img src={twitter} alt="" />
+                <img src={instagram} alt="" />
+            </div>
         </footer>
     )
 }
