c4560102527039e79ea5cceb7e7ef7217129f511
Gilad Peltz <gilad.peltz@gmail.com>
2021-05-24 15:26:18 +0300

    guests modal in progress

--- src/cmps/GuestModal/GuestModal.jsx
+++ src/cmps/GuestModal/GuestModal.jsx
@@ -1,16 +1,33 @@
+import { GuestModalPreview } from '../GuestModalPreview/GuestModalPreview';
+import './GuestModal.scss';
 
-
-import { GuestModalPreview } from '../GuestModalPreview/GuestModalPreview'
-import './GuestModal.scss'
-
-export const GuestModal = ({guestNum, updateNumOfGuests}) => {
-
-    return (
-        <section className="guest-modal">
-            <GuestModalPreview title="Adults" desc="Ages 13 or above" num={guestNum.Adults} updateNumOfGuests={updateNumOfGuests} />
-            <GuestModalPreview title="Children" desc="Ages 2-12" num={guestNum.Children} updateNumOfGuests={updateNumOfGuests} />
-            <GuestModalPreview title="Infants" desc="Under 2" num={guestNum.Infants} updateNumOfGuests={updateNumOfGuests} />
-        </section>
-    )
-}
-
+export const GuestModal = ({ guestNum, updateNumOfGuests, isFixed }) => {
+	return (
+		<section
+			className='guest-modal'
+			style={{
+				position: isFixed ? 'fixed' : 'relative',
+				top: isFixed ? '215px' : '-19%',
+			}}
+		>
+			<GuestModalPreview
+				title='Adults'
+				desc='Ages 13 or above'
+				num={guestNum.Adults}
+				updateNumOfGuests={updateNumOfGuests}
+			/>
+			<GuestModalPreview
+				title='Children'
+				desc='Ages 2-12'
+				num={guestNum.Children}
+				updateNumOfGuests={updateNumOfGuests}
+			/>
+			<GuestModalPreview
+				title='Infants'
+				desc='Under 2'
+				num={guestNum.Infants}
+				updateNumOfGuests={updateNumOfGuests}
+			/>
+		</section>
+	);
+};
