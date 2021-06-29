f0b4b169bd894826404e45c5e9099f7527cb0f4d
zina <zinam84@gmail.com>
2021-06-29 16:17:59 +0300

    stay edit select multiple

--- src/pages/StayEdit/StayEdit.jsx
+++ src/pages/StayEdit/StayEdit.jsx
@@ -4,22 +4,49 @@ import { useDispatch, useSelector } from 'react-redux';
 import { stayService } from '../../services/stayService';
 import { countryCodes } from '../../services/countryService';
 import { saveStay } from '../../store/actions/stayActions';
-
 import { ImageUploader } from '../../cmps/ImageUploader';
-
 import { Input } from '../../cmps/Input';
-
+// import Input from '@material-ui/core/Input';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import Select from '@material-ui/core/Select';
+// import Checkbox from '@material-ui/core/Checkbox';
 import './StayEdit.scss';
 
 export const StayEdit = () => {
-	const [stay, setStay] = useState(null);
-	const [errMsg, setErrMsg] = useState('');
-
-	const [urls, setUrls] = useState([]);
-
 	const dispatch = useDispatch();
 	const history = useHistory();
 	const params = useParams();
+
+	const [stay, setStay] = useState(null);
+	const [errMsg, setErrMsg] = useState('');
+	const [urls, setUrls] = useState([]);
+	const [amenities, setAmenities] = useState([]);
+	const allAmenities = [
+		'TV',
+		'Wifi',
+		'Kitchen',
+		'Hangers',
+		'Smoking allowed',
+		'Pets allowed',
+		'Cooking basics',
+		'Free parking on premises',
+		'Indoor fireplace',
+		'Heating',
+		'Air conditioning'
+	]
+
+	const ITEM_HEIGHT = 48;
+	const ITEM_PADDING_TOP = 8;
+	const MenuProps = {
+		PaperProps: {
+			style: {
+				maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+				width: 250,
+			},
+		},
+	};
 
 	useEffect(() => {
 		const { id } = params;
@@ -28,7 +55,6 @@ export const StayEdit = () => {
 				const stay = id
 					? await stayService.getById(id)
 					: stayService.getEmptyStay();
-				console.log(stay);
 				setStay(stay);
 			} catch (err) {
 				setErrMsg('Stay Not Found');
@@ -62,6 +88,10 @@ export const StayEdit = () => {
 		setStay({ ...stay, [field]: value });
 	};
 
+	const handleChangeSelect = (event) => {
+		setAmenities(event.target.value);
+	};
+
 	const onClearHandler = async () => {
 		const initialState = await stayService.getEmptyStay();
 		setStay(initialState);
@@ -79,7 +109,7 @@ export const StayEdit = () => {
 
 	const getTitle = () => {
 		const { id } = params;
-		return id ? 'Edit Current Stay' : 'Add New Stay';
+		return id ? 'Edit Stay' : 'Add New Stay';
 	};
 
 	const setCountryCode = () => {
@@ -93,14 +123,13 @@ export const StayEdit = () => {
 	};
 
 	return (
-		<React.Fragment>
+		<section className='stay-edit main-layout'>
 			<h1>{getTitle()}</h1>
 			{!stay && <div>{errMsg || 'Loading'}</div>}
 			{stay && (
 				<form
-					className='stay-edit'
-					onSubmit={onSaveStay}
 					className='flex-column'
+					onSubmit={onSaveStay}
 				>
 					<Input
 						required
@@ -138,6 +167,23 @@ export const StayEdit = () => {
 						name='country'
 					/>
 					<p>Country Code: {stay.loc.countryCode}</p>
+					{/* <InputLabel id='amenities-select'>Amenities</InputLabel>
+					<Select
+						labelId='amenities-select'
+						multiple
+						value={amenities}
+						onChange={handleChangeSelect}
+						input={<Input />}
+						renderValue={(selected) => selected.join(', ')}
+						MenuProps={MenuProps}
+					>
+						{allAmenities.map((amenity) => (
+							<MenuItem key={amenity} value={amenity}>
+								<Checkbox checked={amenities.indexOf(amenity) > -1} />
+								<ListItemText primary={amenity} />
+							</MenuItem>
+						))}
+					</Select> */}
 
 					<ImageUploader isMultiple={true} urls={urls} setUrls={setUrls} />
 
@@ -145,6 +191,6 @@ export const StayEdit = () => {
 					<button>Save Stay</button>
 				</form>
 			)}
-		</React.Fragment>
+		</section>
 	);
 };
